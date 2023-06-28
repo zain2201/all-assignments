79f9f13dc2fddc4245c6afc2a5595afe228ca7ee
zain2201 <zain90651@gmail.com>
2023-06-28 18:25:33 +0530

    solved anagram

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,36 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const map1 = new Map();
+  let res = true;
+  str1.split("").forEach((letter) => {
+    if (letter != " ") {
+      if ((letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z"))
+        letter = letter.toLowerCase();
+      let prevCount = map1.get(letter);
+      if (prevCount === undefined) {
+        map1.set(letter, 1);
+      } else map1.set(letter, prevCount + 1);
+    }
+  });
+  console.log(map1);
+  str2.split("").forEach((letter) => {
+    if (letter != " ") {
+      if ((letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z"))
+        letter = letter.toLowerCase();
+      let prevCount = map1.get(letter);
+      if (prevCount === undefined) {
+        map1.set(letter, -1);
+      } else map1.set(letter, prevCount - 1);
+    }
+  });
+  map1.forEach((value) => {
+    if (value > 0 || value < 0) {
+      res = false;
+      return;
+    }
+  });
+  return res;
 }
-
+isAnagram("black car", "car black");
 module.exports = isAnagram;
